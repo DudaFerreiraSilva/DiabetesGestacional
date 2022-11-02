a130d225f7d7ac7cad14b0558416ca957675de58
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-11-01 22:26:37 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿int glicemia;
+string diagnostico;
+ConsoleColor corExibicao;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("--- Diabetes Gestacional ---\n");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Glicemia (em mg/dL)...: ");
+glicemia = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+if (glicemia > 126)
+{
+    diagnostico = "Diabetes Mellitus na gravidez";
+    corExibicao = ConsoleColor.Red;
+}
+else
+{
+    if (glicemia >= 92)
+    {
+        diagnostico = "Compatível com Diabetes Gestacional";
+        corExibicao = ConsoleColor.Yellow;
+    }
+    else
+    {
+        diagnostico = "Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose";
+        corExibicao = ConsoleColor.Blue;
+    }
+}
+
+Console.ForegroundColor = corExibicao;
+
+Console.WriteLine();
+Console.WriteLine(diagnostico);
+
+Console.ResetColor();
